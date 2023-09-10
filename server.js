4ebae7ca4138a0d458a4b6007346e425d350733f
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2023-09-10 13:38:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3001; // Choose any available port number
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  const formData = req.body;
+  // Process the formData as needed (e.g., save to a database)
+  console.log('Received form data:', formData);
+
+  // Send a response back to the client
+  res.json({ message: 'Form data received successfully' });
 });
 
 app.listen(port, () => {
