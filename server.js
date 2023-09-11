9a6841fd3858a69be412c42e0a324630de26b2b0
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2023-09-11 15:29:51 -0400

    make port dynamic

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./db'); // Import your db.js file
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
